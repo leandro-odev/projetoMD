1b6356e8b6c98e3d5bbdfbed5b4e5d3448772bf9
Arthur Sean C. Campos <113319924+arthursean@users.noreply.github.com>
2023-04-22 04:28:08 -0300

    Add files via upload

---
+++ descriptografia.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int exp_mod_rapida(int mensagem, int e, int n)
+{
+    int res = 1;
+    while (e > 0)
+    {
+        if (e % 2 == 1)
+        {
+            res = (res * mensagem) % n;
+        }
+        mensagem = (mensagem * mensagem) % n;
+        e = e / 2;
+    }
+    return res;
+}
+long long int euclidesExtendido(long long int a, long long int b, long long int *s, long long int *t) // Achar o 'd'
+{
+    if (b == 0)
+    {
+        *s = 1;
+        *t = 0;
+        return a;
+    }
+    long long int s1, t1;
+    int mdc = euclidesExtendido(b, a % b, &s1, &t1);
+    *s = t1;
+    *t = s1 - (a / b) * t1;
+    return mdc;
+}
+long long int chavePrivada(long long int a, long long int m) // Enviar o 'd' da chave privada
+{
+    long long int s, t;
+    int mdc = euclidesExtendido(a, m, &s, &t);
+    if (mdc != 1)
+    {
+        return -1;
+    }
+    return (s % m + m) % m;
+}
+int main()
+{
+    int e, p, q;
+    printf("Digite a chave publica: ");
+    scanf("%d %d %d", &p, &q, &e);
+    int mensagem;
+    int d = chavePrivada(e, ((p - 1) * (q - 1)));
+    char oi = '\n';
+    int parada = oi;
+    while (scanf("%d", &mensagem) != parada)
+    {
+        int resultado = mensagem;
+        resultado = exp_mod_rapida(mensagem, d, p * q);
+        char letra = resultado + 63;
+        printf("%c", letra);
+    }
+    return 0;
+}
